c7fe91c719ed3edb28f3e37755a13da09fce0bd1
Sulaiman Kasas <Sulaiman96@outlook.dk>
2019-11-12 11:49:44 +0100

    Vi er ved at lave chancekortene

---
+++ src/main/java/model/CCD_Controller.java
@@ -0,0 +1,16 @@
+package model;
+
+public class CCD_Controller {
+    private ChanceCardDeck chanceCardDeck;
+    private String[] chanceCardTexts = new String[20];
+
+    public CCD_Controller(){
+      this.chanceCardDeck = new ChanceCardDeck(chanceCardTexts);
+    }
+
+    public void swap(int a, int b){
+
+
+    }
+
+}
--- src/main/java/model/ChanceCardDeck.java
+++ src/main/java/model/ChanceCardDeck.java
@@ -1,5 +1,17 @@
 package model;
 
 public class ChanceCardDeck {
-    private String[] chanceCardTexts = new String[20];
+    private ChanceCard[] chanceCards;
+
+    public ChanceCardDeck(String[] chanceCardTexts){
+        this.chanceCards = new ChanceCard[chanceCardTexts.length];
+
+        for (int i=0;i<chanceCardTexts.length;i++){
+            chanceCards[i] = new ChanceCard(chanceCardTexts[i],i);
+        }
+    }
+
+
+
+
 }
