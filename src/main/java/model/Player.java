8b198e502a707e3d626ac286a050c9dba5ef9fec
Christian <christiankyed@hotmail.com>
2019-11-18 13:49:24 +0100

    Oprettet Player og Account klasserne

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Player {
+    private Account account = new Account(0); //Opretter ny spiller. Spillerens start værdi varierer efter hvor mange spiller der er i spillet
+    private String name;
+
+    public Player(String name, int pointStart){
+        this.account.setBalance(pointStart);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
